31164c7a21e704a65aa02342c38e22e6f4a20cae
Manish <manish97ravula@gmail.com>
2015-09-02 18:19:28 +0530

    Data Acquisition succesful

--- getdata.py
+++ getdata.py
@@ -4,61 +4,45 @@ import numpy as np
 def get_test_data():
     name = 'data'
     tot_data = sp.loadmat(name)
-
-    tot_data = [value for key,value in tot_data.items()];
-
-    tot_data = np.array(tot_data);
-    tot_data = np.delete(tot_data,[1,11],axis=0)
-    test_data = tot_data[0:10];
-    train_data=tot_data[10:];
+    sorted_labels = sorted(tot_data);
+    test_data = np.array([tot_data[label] for label  in sorted_labels[3:13]]);
+    train_data = np.array([tot_data[label] for label in sorted_labels[13:23]]);
     n_classes = 10;
     n_dims = 60;
     return(test_data,train_data)
 
 def vert_cat(Data,n_classes,n_dims):
-    sizes_dataelems = np.array([(np.shape(ele))[0] for ele in Data]);
-    no_samples = np.sum(sizes_dataelems);
-    alldata_samples = np.empty(no_samples,n_dims):
-    indx =0;
-    for i in range(n_clasess):
-        alldata_samples[indx:indx+sizes_dataelems[i]] = Data[i];
-        indx = indx+sizes_dataelems[i];
-    if(indx-1==no_samples):
-        print('success!')
-    return(alldata_samples)
+    alldata_samples = Data[0];
+    for i in range(1,n_classes):
+        alldata_samples = np.vstack((alldata_samples,Data[i]));
+    return alldata_samples
 
 def get_labels(Data,n_classes):
     sizes_dataelems = np.array([(np.shape(ele))[0] for ele in Data]);
     no_samples = np.sum(sizes_dataelems);
-    labels = np.empty(no_samples);
+    labels = np.empty((no_samples,1));
     indx = 0;
     for i in range(n_classes):
-        labels[indx:indx+sizes_dataelems[i]] = i*(np.ones(sizes_dataelems[i]));
+        labels[indx:indx+sizes_dataelems[i],0] = i*(np.ones(sizes_dataelems[i]));
         indx = indx+sizes_dataelems[i];
     return labels;
 
+def receive_data():
+    (test_data,train_data) = get_test_data()
+
+    test_labels = get_labels(test_data,10);
+    train_labels = get_labels(train_data,10);
+
+    all_test_data = vert_cat(test_data,10,60);
+    all_train_data = vert_cat(train_data,10,60);
+
+    all_test_data = np.hstack((all_test_data,test_labels));
+    all_train_data = np.hstack((all_test_data,test_labels));
+
+    return((all_train_data,all_test_data))
 
 
 
 
 
 
-
-    """
-    sizes_testclasses = [(np.shape(ele))[0] for ele in test_data];
-    sizes_testclasses = np.array(sizes_testclasses);
-    n_testeles = np.sum(sizes_testclasses);
-    test_labels = np.empty(n_testeles)
-    indx = 0
-    for i in range(n_classes):
-        test_labels[indx:indx+sizes_testclasses[i]] = i*(np.ones(sizes_testclasses[i]));
-        indx = indx+sizes_testclasses[i];
-    #[no_samples,no_features] = np.size(test_data);
-    test_data_final = test_data[0]
-    for ele in test_data[1:]:
-        test_data_final = np.vstack((test_data_final,ele));
-    return(test_data,test_labels)
-"""
-(test_data,train_data) = get_test_data();
-q = get_labels(test_data,10)
-print(test_data,q)
